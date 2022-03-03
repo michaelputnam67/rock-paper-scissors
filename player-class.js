9b610b0a99ad1ad571952b030f96cd55ca0b96b9
Michael Putnam <mikeputnam713@gmail.com>
2022-03-02 22:10:44 -0800

    Add initial class to class functionality

---
+++ player-class.js
@@ -0,0 +1,17 @@
+class Player {
+	constructor(name, weapon, avatar) {
+		this.name = name || 'Michael';
+		this.token = avatar || 'img';
+		this.wins = 0;
+		this.weapon = weapon 
+	}	
+
+	// chooseWeapon(weapon) {
+	// 	this.weapon = weapon
+	// }
+
+
+	takeTurn() {
+
+	}
+}
