f7ffe868a08611dd974ac519e8c0e9ffadb75aa7
Michael Putnam <mikeputnam713@gmail.com>
2022-03-03 21:46:56 -0800

    Update DOM JS integration

--- assets/data.js
+++ assets/data.js
@@ -4,13 +4,32 @@ var classic = [
 	{name: "paper", strongAgainst: ['rock']}
 ]
 
-var alternate = [
-	{name: 'heart', strongAgainst: ['water', 'earth']},
-	{name: 'water', strongAgainst: ['fire', 'earth']},
-	{name: 'earth', strongAgainst: ['fire', 'wind']},
-	{name: 'fire', strongAgainst: ['heart', 'wind']},
-	{name: 'wind', strongAgainst: ['water', 'heart']},
-]
+
+var alternate = {
+	section: 'div',
+	section1: [
+		{img:'assets/Icons/earth.png', alt:'rock icon'},
+		{img:'assets/Icons/flames.png', alt:'flames icon'},
+		{img:'assets/Icons/wind.png', alt:'wind icon'},
+		{img:'assets/Icons/water.png', alt:'water icon'},
+		{img:'assets/Icons/heart.png', alt:'heart icon'}
+	],
+	weapons: 
+	[	{name: 'heart', strongAgainst: ['water', 'earth']},
+		{name: 'water', strongAgainst: ['fire', 'earth']},
+		{name: 'earth', strongAgainst: ['fire', 'wind']},
+		{name: 'fire', strongAgainst: ['heart', 'wind']},
+		{name: 'wind', strongAgainst: ['water', 'heart']},]
+
+}
+	
+// var alternate = [
+// 	{name: 'heart', strongAgainst: ['water', 'earth']},
+// 	{name: 'water', strongAgainst: ['fire', 'earth']},
+// 	{name: 'earth', strongAgainst: ['fire', 'wind']},
+// 	{name: 'fire', strongAgainst: ['heart', 'wind']},
+// 	{name: 'wind', strongAgainst: ['water', 'heart']},
+// ]
 
 var avatar = [
 	'./assets/Icons/chick.png',
--- script.js
+++ script.js
@@ -20,14 +20,24 @@ var avatarButton = document.querySelector('.change-avatar')
 var generateUserButton = document.querySelector('.generate-user')
 var userNameInput = document.querySelector('.user-name')
 var formAvatarIcon = document.querySelector('.avatar-icon')
+var alternateButton = document.getElementById('alternate')
+var classicButton = document.getElementById('classic')
+var topDisplay = document.getElementById('homeTopView')
+var bottomDisplay = document.getElementById('playerDisplay')
+var main = document.getElementById('main')
+
 
 //~~~~~~~~~~~~~~~~~~EVENT LISTENERS~~~~~~~~~~~~~~~~~~~~~~~~~~~
-window.addEventListener('load', displayForm)
+// window.addEventListener('load', displayForm)
 avatarButton.addEventListener('click', changeFormAvatarIcon)
 generateUserButton.addEventListener('click', function() {
 	event.preventDefault()
 	generatePlayerOne()
 })
+alternateButton.addEventListener('click', function(){
+	switchToAlternateBattleWindow()
+})
+
 
 
 //~~~~~~~~~~~~~~~~~~~FUNCTIONS~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -62,3 +72,48 @@ function populateUserData() {
 		document.getElementById('computerScore').innerText = `Wins = ${currentPlayers[1].score}`;
 	}
 }
+
+function switchToAlternateBattleWindow() {
+	topDisplay.classList.add('hidden')
+	var battleSection = document.createElement('section')
+	battleSection.id = 'battleInterface'
+	battleSection.classList.add('top-display')
+	addHeader(battleSection)
+	addGameIcons(battleSection)
+	for(var i = 0; i < 5; i++) {
+
+	}
+	main.insertBefore(battleSection, bottomDisplay)
+	addDisplayEventListener()	
+}
+
+function addDisplayEventListener() {
+	var battleInterface = document.getElementById('battleInterface')
+	battleInterface.addEventListener('click', function() {
+		test()
+	})
+}
+
+function test() {
+	console.log('test')
+}
+
+function addHeader(battleSection) {
+	var header = document.createElement('h1')
+	header.innerText = 'Hello World!'
+	battleSection.appendChild(header)
+}
+
+function addGameIcons(battleSection) {
+	var icons = document.createElement('div')
+	icons.classList.add('game-view-weapons')
+	for(var i = 0; i < alternate.section1.length; i++) {
+		var test = document.createElement('img')
+		test.src = alternate.section1[i].img
+		test.alt = alternate.section1[i].alt
+		test.classList.add('figures-weapon')
+		icons.appendChild(test)
+	}
+	battleSection.appendChild(icons)
+}
+
